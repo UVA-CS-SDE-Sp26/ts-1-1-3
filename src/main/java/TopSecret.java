6bba8992aa10ed2794b0a03583ecdd0b2b25f71e
wbltrn <143052200+wbltrn@users.noreply.github.com>
2026-02-12 18:11:39 -0500

    Implement UserInterface run logic and update documentation

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -5,7 +5,14 @@ public class TopSecret {
 
     public static void main(String[] args){
 
+       // create program control that has file access, data retrieval, and deciphering logic
+        ProgramControl controller = new ProgramControl();
 
+        //create user interface object and give it access to program control so it can request data
+        UserInterface ui = new UserInterface(controller);
+
+        //start program by passing arguments
+        ui.run(args);
 
     } //main method
 
