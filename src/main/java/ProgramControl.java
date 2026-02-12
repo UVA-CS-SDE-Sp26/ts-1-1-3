8d769dc5c8beb86acdf92895dc66246f26d34275
Nate <cowcorner005@gmail.com>
2026-02-11 19:03:25 -0500

    getNumberedFileList() made, throws FileNotFoundException if no 'data' folder

--- src/main/java/ProgramControl.java
+++ src/main/java/ProgramControl.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProgramControl {
@@ -10,9 +13,23 @@ public class ProgramControl {
      *
      * @return numbered list of available files for display
      */
-    public List<String> getNumberedFileList(){
+    public List<String> getNumberedFileList() throws FileNotFoundException {
         //Implemented by Member B
-        return null;
+
+            File dataFolder = new File("data");
+            File[] dataFileList = dataFolder.listFiles();
+            if (dataFileList == null){
+                throw new FileNotFoundException("Data folder is null");
+            }
+
+            else {
+                List<String> NumberedFileList = new ArrayList<>();
+                for (int i = 0; i < dataFileList.length; i++) {
+                    NumberedFileList.add((i + 1) + " " + dataFileList[i].getName());
+                }
+                return NumberedFileList;
+            }
+
     }
 
     /**
